a228c143216dc5e34fc9a013e0225533bd9f5060
reokoizumi <reokoizumi0930@gmail.com>
2023-03-27 14:43:11 +0900

    deploy設定の追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteSingleFile } from "vite-plugin-singlefile";
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [
+    react(), 
+    viteSingleFile(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'gas/*',
+          dest: './'
+        }
+      ]
+    })
+  ],
 })
