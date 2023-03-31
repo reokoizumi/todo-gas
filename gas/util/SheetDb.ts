3a44c02a5c178286d46a349e36bb5248b78edb44
reokoizumi <reokoizumi0930@gmail.com>
2023-03-31 09:39:53 +0900

    使用しなかったため削除

--- gas/util/SheetDb.ts
+++ gas/util/SheetDb.ts
@@ -41,25 +41,6 @@ export class SheetDb {
     sheet.appendRow(values)
   }
 
-    /**
-    * 該当するidの行番号を取得
-    * @param sheetName シート名
-    * @param rowIndex 行のインデックス
-    * @param values 1行分の配列
-    * @return void
-    */
-  getRowIndex(sheetName: string, id: string) {
-    const sheet: Sheet = this.getSheet(sheetName)
-    const record = sheet.getRange(1, 2, sheet.getLastRow()).getValues()
-    const rowIndex = record.flat().filter((value, index) => (value === id) ? index : null)
-    if (!rowIndex) {
-      console.error(`${id}が存在しません`)
-      return
-    }
-    return rowIndex
-
-  }
-
   /**
     * 行の更新
     * @param sheetName シート名
